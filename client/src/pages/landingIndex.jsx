759db4259ffbc9f83a2a10d3560ec3029883bed2
Ayush Mishra <ayushmishra.5a@gmail.com>
2026-02-09 22:55:06 +0530

    Added client folder

---
+++ client/src/pages/landingIndex.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function landingIndex() {
+  return <div>landingIndex</div>;
+}
+
+export default landingIndex;
---
+++ client/src/sampleRegister.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+//Sample page for registering new users
+function sampleRegister() {
+  return <div>sampleRegister</div>;
+}
+
+export default sampleRegister;
