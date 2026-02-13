e84aa16238e741ab1573a65f4a732b655fdedf63
Rajdeep <rajdeep2878483@gmail.com>
2026-02-14 01:35:16 +0530

    landing size fixed

--- client/src/pages/Sidebar.jsx
+++ client/src/pages/Sidebar.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "../styles/sidebar.css";
+import "../styles/Sidebar.css";
 import Logout from "./Logout";
 
 const Sidebar = () => {
